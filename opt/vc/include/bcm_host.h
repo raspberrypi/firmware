ea493c30e2c5328b80943dff2db909f194cdf155
popcornmix <popcornmix@gmail.com>
2012-03-22 23:33:06 +0000

    Update to LKG21 and rebuild of hardfp libs. Tidy up unwanted/obsolete

--- opt/vc/include/bcm_host.h
+++ opt/vc/include/bcm_host.h
@@ -1,23 +1,24 @@
 /*
- *  host_applications/linux/libs/include/bcm_host.h
+ * Copyright (c) 2012 Broadcom Europe Ltd
  *
- *  Copyright (C) 2010 Broadcom
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Header file with useful bits from other headers
  */
-
 
 #ifndef BCM_HOST_H
 #define BCM_HOST_H
@@ -1060,6 +1061,8 @@ VCHPRE_ int VCHPOST_ vc_cec_send_Standby(uint32_t follower, bool_t is_reply);
 VCHPRE_ int VCHPOST_ vc_cec_send_MenuStatus(uint32_t follower, 
                                             CEC_MENU_STATE_T menu_state, 
                                             bool_t is_reply);
+
+VCHPRE_ int VCHPOST_ vc_gencmd(char *response, int maxlen, const char *format, ...);
 
 #ifdef __cplusplus
 }
