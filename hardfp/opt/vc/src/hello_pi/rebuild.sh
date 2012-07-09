caed4bf024f92322161a97ca39a740c02f9f167f
popcornmix <popcornmix@gmail.com>
2012-07-09 21:33:03 +0100

    Updating dwc_otg driver to fix issue releasing pcm stream Backport of Chris Boot's i2c and spi drivers. Firmware has hdmi_ignore_cec config option added. Determine board revision from OTP in preference to GPIO version Fix for hdmi_ignore_edid not allowing unsupported modes. Tidy up of /opt/vc files and hello_pi to build with raspbian

---
+++ hardfp/opt/vc/src/hello_pi/rebuild.sh
@@ -0,0 +1,21 @@
+make -C libs/ilclient clean
+make -C libs/vgfont clean
+make -C hello_world clean
+make -C hello_triangle clean
+make -C hello_triangle2 clean
+make -C hello_video clean
+make -C hello_audio clean
+make -C hello_font clean
+make -C hello_dispmanx clean
+
+make -C libs/ilclient
+make -C libs/vgfont
+make -C hello_world
+make -C hello_triangle
+make -C hello_triangle2
+make -C hello_video
+make -C hello_audio
+make -C hello_font
+make -C hello_dispmanx
+
+
