e73e84c7229d20026b5c07fe625cc881c5475c8c
popcornmix <popcornmix@gmail.com>
2012-07-25 22:21:04 +0100

    Revert unwanted change to licence header

--- hardfp/opt/vc/include/interface/vmcs_host/vc_cec.h
+++ hardfp/opt/vc/include/interface/vmcs_host/vc_cec.h
@@ -1,32 +1,34 @@
 /*
-* Copyright (c) 2012 Broadcom Europe Ltd
-*
-* Licensed to the Apache Software Foundation (ASF) under one or more
-* contributor license agreements.  See the NOTICE file distributed with
-* this work for additional information regarding copyright ownership.
-* The ASF licenses this file to You under the Apache License, Version 2.0
-* (the "License"); you may not use this file except in compliance with
-* the License.  You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*
-* CEC related constants - shared by both host and vc.
+Copyright (c) 2012, Broadcom Europe Ltd
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the copyright holder nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
+
+// CEC related constants - shared by both host and vc.
 
 #ifndef _VC_CEC_H_
 #define _VC_CEC_H_
-
-#ifndef STRINGIFY
-#define STRINGIFY(x) #x
-#define TOSTRING(x)  STRINGIFY(x)
-#endif
-
 //Broadcast address and TV logical address
 #define CEC_BROADCAST_ADDR       0x0F
 #define CEC_TV_ADDRESS           0x00
@@ -480,18 +482,4 @@ typedef void (*CEC_CALLBACK_T)(void *client_p, uint32_t reason, uint32_t param1,
 #define CEC_CB_OPCODE(x) (((x) >> 8) & 0xFF) /** Get the opcode from first parameter */
 #define CEC_CB_OPERAND1(x) (((x) >> 16) & 0xFF) /** Get the button code from <User Control Pressed> or the first operand of the opcode */
 #define CEC_CB_OPERAND2(x) (((x) >> 24) & 0xFF) /** Get the second operand of opcode */
-
-//CEC service return code
-typedef enum {
-   VC_CEC_SUCCESS                = 0, /** OK */
-   VC_CEC_ERROR_NO_ACK           = 1, /** No acknowledgement */
-   VC_CEC_ERROR_SHUTDOWN         = 2, /** In the process of shutting down */
-   VC_CEC_ERROR_BUSY             = 3, /** block is busy */
-   VC_CEC_ERROR_NO_LA            = 4, /** No logical address */
-   VC_CEC_ERROR_NO_PA            = 5, /** No physical address */
-   VC_CEC_ERROR_NO_TOPO          = 6, /** No topology */
-   VC_CEC_ERROR_INVALID_FOLLOWER = 7, /** Invalid follower */
-   VC_CEC_ERROR_INVALID_ARGUMENT = 8  /** Invalid arguments */
-} VC_CEC_ERROR_T;
-
 #endif
